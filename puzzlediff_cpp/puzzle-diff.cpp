c3b8ed8ae63f4cfdcd20d3eac8886dc0a8743da9
gaoyuan8827 <gaoyuan8827@gmail.com>
2014-11-15 20:31:36 +0200

    Revert "Test commit"
    This reverts commit 6677e902b3978cbc0ae07412b6196eeba8f36991.

--- puzzlediff_cpp/puzzle-diff.cpp
+++ puzzlediff_cpp/puzzle-diff.cpp
@@ -59,7 +59,6 @@ int parse_opts(Opts * const opts, PuzzleContext * context,int argc, char **argv)
 
 int main(int argc, char *argv[])
 {
-    cout << "Test" << endl;
     Opts opts;
     PuzzleContext context;
     PuzzleCvec cvec1, cvec2;
