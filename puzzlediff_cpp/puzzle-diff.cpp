2ae4d586e7a31d54ef4bbe77d37ba52b6e4cedbd
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2014-11-15 18:36:22 +0200

    feature: proper program signature

--- puzzlediff_cpp/puzzle-diff.cpp
+++ puzzlediff_cpp/puzzle-diff.cpp
@@ -5,37 +5,21 @@ extern "C" {
 
 #include "pgetopt.hpp"
 #include "listdir.hpp"
+#include <iostream>
 
 using namespace std;
 
 typedef struct Opts_ {
-    const char *file1;    
+    const char *inputFile;
+    const char *outputFile;
 	vector<string> directory;
-    int fix_for_texts;
     int exit;
     double similarity_threshold;
 } Opts;
 
 void usage(void)
 {
-    puts("\nUsage: puzzle-diff [-b <contrast barrier for cropping] [-c]\n"
-         "   [-C <max cropping ratio>] [-e] [-E similarity threshold] [-h]\n"
-         "   [-H <max height>] [-l <lambdas>] [-n <noise cutoff>]\n"
-         "   [-p <p ratio>] [-t] [-W <max width>] <file 1> <file 2>\n\n"
-         "Visually compares two images and returns their distance.\n\n"
-         "-b <contrast barrier for cropping>\n"
-         "-c : disable autocrop\n"
-         "-C <max cropping ratio>\n"
-         "-e : exit with 10 (images are similar) or 20 (images are not)\n"
-         "-E <similarity threshold> : for -e\n"
-         "-h : show help\n"
-         "-H <width> : set max height\n"
-         "-l <lambdas> : change lambdas\n"
-         "-n <noise cutoff> : change noise cutoff\n"
-         "-p <ratio> : set p ratio\n"
-         "-t disable fix for texts\n"
-         "-W <width> : set max width\n"
-         "\n");
+    puts("Usage: puzzle-diff [-o <output file>] <image to compare against> <directory with images for comparison>");
     exit(EXIT_SUCCESS);
 }
 
@@ -45,58 +29,28 @@ int parse_opts(Opts * const opts, PuzzleContext * context,int argc, char **argv)
     extern char *poptarg;
     extern int poptind;
 
-    opts->fix_for_texts = 1;
     opts->exit = 0;
     opts->similarity_threshold = PUZZLE_CVEC_SIMILARITY_THRESHOLD;
-    while ((opt = pgetopt(argc, argv, "b:cC:eE:hH:l:n:p:tW:")) != -1) {
-        switch (opt) {
-        case 'b':
-            puzzle_set_contrast_barrier_for_cropping(context, atof(poptarg));
-            break;
-        case 'c':
-            puzzle_set_autocrop(context, 0);
-            break;            
-        case 'C':
-            puzzle_set_max_cropping_ratio(context, atof(poptarg));
-            break;
-        case 'e':
-            opts->exit = 1;
-            break;
-        case 'E':
-            opts->similarity_threshold = atof(poptarg);
-            break;
-        case 'h':
-            usage();
-            /* NOTREACHED */
-        case 'H':
-            puzzle_set_max_height(context, strtoul(poptarg, NULL, 10));
-            break;
-        case 'l':
-            puzzle_set_lambdas(context, strtoul(poptarg, NULL, 10));
-            break;
-        case 'n':
-            puzzle_set_noise_cutoff(context, atof(poptarg));
-            break;
-        case 'p':
-            puzzle_set_p_ratio(context, atof(poptarg));
-            break;
-        case 't':
-            opts->fix_for_texts = 0;
-            break;
-        case 'W':
-            puzzle_set_max_width(context, strtoul(poptarg, NULL, 10));
-            break;
-        default:
-            usage();
-            /* NOTREACHED */            
-        }
-    }
-    argc -= poptind;
-    argv += poptind;
-    if (argc != 2) {
+
+    if (argc < 3 || argc > 5) {
         usage();
+        return -1;
     }
-    opts->file1 = *argv++;
+
+    *argv++;
+
+    if (strcmp(argv[0], "-o") == 0) {
+    	*argv += 2;
+
+    	if (argv[0][0] == '\0') {
+    		*argv++;
+    	}
+
+    	opts->outputFile = *argv;
+        *argv++;
+    }
+
+    opts->inputFile = *argv++;
     // filling the vector inside opts struct with found files in directory
 	listDir(*argv, opts->directory);
     
@@ -113,6 +67,9 @@ int main(int argc, char *argv[])
     puzzle_init_context(&context);  
     parse_opts(&opts, &context, argc, argv);
 
+    for (uint i = 0; i < opts.directory.size(); i++) {
+    	cout << opts.directory[i] << endl;
+    }
 
     /*puzzle_init_cvec(&context, &cvec1);
     puzzle_init_cvec(&context, &cvec2);
