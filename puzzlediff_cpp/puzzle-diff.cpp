90fffe14f01624f169c2076c2ae1c5a48376748e
Igor Suleymanoff <igorsuleymanoff@gmail.com>
2014-11-30 16:10:51 +0200

    feature: code the final output format

--- puzzlediff_cpp/puzzle-diff.cpp
+++ puzzlediff_cpp/puzzle-diff.cpp
@@ -3,20 +3,21 @@ extern "C" {
   #include "puzzle.h"
   #include <time.h>
 }
+
 #include "pgetopt.hpp"
 #include "listdir.hpp"
-#include "advisor-annotate.h"
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 typedef struct Opts_ {
-    const char *inputFile;
+  std::vector <std::string> directory;
+
+  const char *inputFile;
     const char *outputFile;
     const char *dirName;
-	vector<string> directory;
-    int exit;
-	int fix_for_texts;
+
     double similarity_threshold;
 } Opts;
 
@@ -25,16 +26,15 @@ void usage()
     puts("Usage: puzzle-diff [-o <output file>] <image to compare against> <directory with images for comparison>");
 }
 
-int parse_opts(Opts * const opts, PuzzleContext * context,int argc, char **argv) 
+int parse_opts(Opts * const opts, PuzzleContext * context,int argc, char **argv)
 {
     int opt;
     extern char *poptarg;
     extern int poptind;
 
-    opts->exit = 0;
     opts->similarity_threshold = PUZZLE_CVEC_SIMILARITY_THRESHOLD;
 
-    if (argc < 3 || argc > 5) 
+    if (argc < 3 || argc > 5)
     {
         usage();
         return 0;
@@ -42,61 +42,90 @@ int parse_opts(Opts * const opts, PuzzleContext * context,int argc, char **argv)
     // jumping first call argument (program name)
     *argv++;
 
-    if (!strcmp(argv[0], "-o")) 
+    if (!strcmp(argv[0], "-o"))
     {
-		//jump to file name
-    	*argv ++;
-    	opts->outputFile = *argv++;
+    //jump to file name
+      *argv ++;
+      opts->outputFile = *argv++;
     }
 
     opts->inputFile = *argv++;
     opts->dirName = *argv;
     // filling the vector inside opts struct with found files in directory
-	listDir(opts->dirName, opts->directory);
-    
+  listDir(opts->dirName, opts->directory);
+
     return 1;
+}
+
+std::pair<double, string> compare_image_files(PuzzleContext puzzle_context, PuzzleCvec original_vector, PuzzleCvec comparison_vector, string file_name)
+{
+  double distance = 10;
+
+  puzzle_init_cvec(&puzzle_context, &comparison_vector);
+
+  if (puzzle_fill_cvec_from_file(&puzzle_context, &comparison_vector, file_name.c_str())) {
+    cerr << "Unable to read comparison file: " << file_name << endl;
+  }
+  else {
+    distance = puzzle_vector_normalized_distance(&puzzle_context, &original_vector, &comparison_vector, 1);
+  }
+
+  puzzle_free_cvec(&puzzle_context, &comparison_vector);
+
+  return std::pair<double, std::string>(distance, file_name);
 }
 
 int main(int argc, char *argv[])
 {
     Opts opts;
-    PuzzleContext context;
-    PuzzleCvec cvec1, cvec2;
-    double d;
-	double start, end;
-    
-    puzzle_init_context(&context);
-	opts.fix_for_texts = 1;
+    PuzzleContext puzzle_context;
+    PuzzleCvec original_vector, comparison_vector;
+    std::multimap <double, std::string, std::less<double> > results;
 
-    if(!parse_opts(&opts, &context, argc, argv))
+    puzzle_init_context(&puzzle_context);
+
+    if (!parse_opts(&opts, &puzzle_context, argc, argv)) {
         exit(EXIT_FAILURE);
+    }
 
-	start = clock();
+  puzzle_init_cvec(&puzzle_context, &original_vector);
 
-	puzzle_init_cvec(&context, &cvec1);
-    
-	if (puzzle_fill_cvec_from_file(&context, &cvec1, opts.inputFile)) {
-		fprintf(stderr, "Unable to read [%s]\n", opts.inputFile);
-		return 1;
-	}
+  if (puzzle_fill_cvec_from_file(&puzzle_context, &original_vector, opts.inputFile)) {
+    cerr << "Unable to read input file: " << opts.inputFile << endl;
+    exit(EXIT_FAILURE);
+  }
 
-	ANNOTATE_SITE_BEGIN(walkDir);
-	for (int i = 0; i < opts.directory.size(); i++)
-	{
-		ANNOTATE_ITERATION_TASK(comparePictures);
-		puzzle_init_cvec(&context, &cvec2);
-		if (puzzle_fill_cvec_from_file(&context, &cvec2, opts.directory[i].c_str()))
-			cerr << "Unable to read " << opts.directory[i] << endl;
-		else
-			cout << puzzle_vector_normalized_distance(&context, &cvec1, &cvec2,opts.fix_for_texts) << endl;
-		puzzle_free_cvec(&context, &cvec2);
-	}
-	ANNOTATE_SITE_END();
+  for (unsigned int i = 0; i < opts.directory.size(); ++i) {
+    std::string file_name = opts.directory[i];
+    std::pair<double, std::string> comparison_result = compare_image_files(puzzle_context, original_vector, comparison_vector, file_name);
 
-    puzzle_free_cvec(&context, &cvec1);
-    puzzle_free_context(&context);
-	end = clock();
+    results.insert(comparison_result);
+  }
 
-	cout << "computation time: " << (double)(end - start) / CLOCKS_PER_SEC << endl;
+    puzzle_free_cvec(&puzzle_context, &original_vector);
+    puzzle_free_context(&puzzle_context);
+
+    unsigned int count = 0;
+    std::multimap <double, std::string>::iterator it;
+    cout << endl << "*** Pictures found to be similar to "<< opts.inputFile << " ***" << endl;
+
+    for (it = results.begin(); it != results.end(); ++it) {
+      if (count > 10) break;
+      cout << (*it).first << ' ' << (*it).second << endl;
+      ++count;
+    }
+
+    count = 0;
+    cout << endl << "*** Pictures found to be identical/close resemblance to "<< opts.inputFile << " ***" << endl;
+
+    for (it = results.begin(); it != results.end(); ++it) {
+      if (count > 10) break;
+
+      if ((*it).first <= 0.12) {
+        cout << (*it).first << ' ' << (*it).second << endl;
+        ++count;
+      }
+    }
+
     return 0;
 }
